01fc4bfddfbbae647a4047788786ee12da2f43b3
inthe991 <2786555831@qq.com>
2026-07-18 16:42:19 +0800

    修了一下工作流

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@
 
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
