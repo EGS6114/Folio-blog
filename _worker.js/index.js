53004c325039485b844d331169e02ed5f0bfa389
EGS6114 <EGS6114@users.noreply.github.com>
2026-07-18 08:55:13 +0000

    deploy: 51f07c128c17da21eef6a7682c42c68da9ab411f

--- _worker.js/index.js
+++ _worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CjGxjm5s.mjs';
-import { manifest } from './manifest_Di3wKGI3.mjs';
+import { manifest } from './manifest_BFI3I1rv.mjs';
 
 const serverIslandMap = new Map();;
 
